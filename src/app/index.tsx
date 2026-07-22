b57b12c39f7f90ec4d41525c87263c3bac42f1d1
gyo82v <gyo82v@gmail.com>
2026-07-22 15:49:24 +0100

    feat: implemet the development build

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-100">
+    <View className="flex-1 items-center justify-center bg-green-100">
       <Text className="text-4xl font-bold text-pink-600">
         Worth It 🚀
       </Text>
